23cf6d844e693c5a583693fd56c2a2d8f3511dc5
marishikaw <90078966+marishikaw@users.noreply.github.com>
2022-01-14 19:08:36 +0900

    [finish]Chapter01

---
+++ Java_practice/src/Chapter01_1_3.java
@@ -0,0 +1,8 @@
+
+public class Chapter01_1_3 {
+	boolean result = true;
+	char favoriteCharacter = '駆';
+	double pi = 3.14;
+	long number = 314159265265853979L;
+	String msg = "ミナトの攻撃！敵に 15 ポイントのダメージを与えた。";
+}
