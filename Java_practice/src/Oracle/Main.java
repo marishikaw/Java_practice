0781c3214d6cecdeb656d9df44a458a6bdb56555
marishikaw <90078966+marishikaw@users.noreply.github.com>
2022-05-28 14:22:47 +0900

    [Update]

--- Java_practice/src/Oracle/Main.java
+++ Java_practice/src/Oracle/Main.java
@@ -9,5 +9,13 @@ public class Main {
 			}
 			System.out.println(" ");
 		}
+		
+		int val = 7;
+		boolean flg = true;
+		if (flg == true) {
+			do {
+					System.out.println(val);
+			} while (val > 10);
+		}
 	}
 }
