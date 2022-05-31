c50c2fa2e049cb2270d4ed109d736610d04ffcb3
marishikaw <90078966+marishikaw@users.noreply.github.com>
2022-05-31 22:05:52 +0900

    [Update]

--- Java_practice/src/Oracle/Main.java
+++ Java_practice/src/Oracle/Main.java
@@ -2,14 +2,21 @@ package Oracle;
 
 public class Main {
 	public static void main(String[] args) {
-		String str = "abcde";
-		System.out.println(str.substring(2,4));
+		int a1 = 3;
+		int b1 = a1 += 5;
+		System.out.println(a1 + b1);
 		
-		String st17 = "Hello, ".concat("Java!");
-		System.out.println(st17);
+		byte a3 = (byte)0b10000000;
+		short b3 = 128 + 128;
+		int c3 = (int)(2 * 3L);
+		float d3 = (float)10.0;
 		
-		String st18 = null;
-		st18 += "null";
-		System.out.println(st18);
+		int a4 = 10;
+		int b4 = a4++ + a4 + a4-- - a4-- + ++a4;
+		System.out.println(b4);
+		
+		Sample a = new Sample(10, "a");
+		Sample b = new Sample(10, "b");
+		System.out.println(a.equals(b));
 	}
 }
