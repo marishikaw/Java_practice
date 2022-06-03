84361656c1f1bbb295158e2f4e21401406fddecb
marishikaw <90078966+marishikaw@users.noreply.github.com>
2022-06-03 21:56:38 +0900

    [Update]

--- Java_practice/src/Oracle/Main.java
+++ Java_practice/src/Oracle/Main.java
@@ -2,37 +2,18 @@ package Oracle;
 
 public class Main {
 	public static void main(String[] args) {
-		String a = "abc";
-		String b = new String(a);
-		
-		int count = 0;
-		if (a.intern() == "abc") {
-			count++;
+		for (int i = 0, j = 0; i < 3 && j< 5; i++) {
+			System.out.println(i++);
+			j += i;
 		}
-		if (b.intern() == "abc") {
-			count++;
-		}
-		if (a.intern() == b.intern()) {
-			count++;
-		}
-		System.out.println(count);
 		
-		int num = 10;
-		if (num <= 10);
-		System.out.println("ok");
-		
-		if(false)
-		System.out.println("A");
-		System.out.println("B");
-		
-		int number = 1;
-		switch (number) {
-		case 1:
-		case 2:
-		case 3: System.out.println("A");
-		case 4: System.out.println("B");
-		default:
-			System.out.println("C");
+		for (int i = 0; i < 3; i++, period()) {
+			System.out.print(i);
 		}
+		
+
+	}
+	private static void period() {
+		System.out.print(",");
 	}
 }
