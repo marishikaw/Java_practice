af4859c0cd066361e03b829092fa0ede3474383f
marishikaw <90078966+marishikaw@users.noreply.github.com>
2022-06-05 20:16:04 +0900

    [Update]

--- Java_practice/src/Oracle/Main.java
+++ Java_practice/src/Oracle/Main.java
@@ -2,18 +2,46 @@ package Oracle;
 
 public class Main {
 	public static void main(String[] args) {
-		for (int i = 0, j = 0; i < 3 && j< 5; i++) {
-			System.out.println(i++);
-			j += i;
+		String[] array = { "A", "B", "C"};
+		for (String str : array) {
+			str = "D";
+		}
+		for (String str : array) {
+			System.out.println(str);
 		}
 		
-		for (int i = 0; i < 3; i++, period()) {
-			System.out.print(i);
-		}
+		int num = 10;
+		do {
+			num++;
+		} while (++num < 10);
+		System.out.println(num);
 		
-
-	}
-	private static void period() {
-		System.out.print(",");
+		num = 10;
+		while (num++ <= 10) {
+			num++;
+		}
+		System.out.println(num);
+		
+		int[] array2 = { 1, 2, 3, 4, 5};
+		int total = 0;
+		for (int i : array2) {
+			if (i % 2 == 0) {
+				continue;
+			}
+			total += i;
+		}
+		System.out.println(total);
+		
+		System.out.println(1 % 2);
+		
+		total = 0;
+		a: for (int i = 0; i < 5; i++) {
+			b: for (int j = 0; j < 5; j++) {
+				if (i % 2 == 0) continue a;
+				if (3 < j) break b;
+				total += j;
+			}
+		}
+		System.out.println(total);
 	}
 }
