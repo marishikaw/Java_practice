d79988c5cd2876ed0c22af6abfbaaac04582a9f1
marishikaw <90078966+marishikaw@users.noreply.github.com>
2022-06-15 21:47:40 +0900

    [Update]

--- Java_practice/src/Chapter10/Main.java
+++ Java_practice/src/Chapter10/Main.java
@@ -1,18 +1,14 @@
 package Chapter10;
 
 public class Main {
-	public static void main(String[] args) {		
-		int result = sample();
-		System.out.println(result);
-	}
-	
-	private static int sample() {
-		try {
-			throw new RuntimeException();
-		} catch (RuntimeException e) {
-			return 10;
-		} finally {
-			return 20;
+	public static void main(String[] args) {
+		main(args);
+		
+		String str = null;
+		if (str.equals("")) {
+			System.out.println("blank");
+		} else {
+			System.out.println("null");
 		}
 	}
 }
