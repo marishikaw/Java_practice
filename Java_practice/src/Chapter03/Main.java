471b7be2225f3911c4d1d9ef4f22a04163fd1712
marishikaw <90078966+marishikaw@users.noreply.github.com>
2022-07-11 20:50:58 +0900

    [Update]

--- Java_practice/src/Chapter03/Main.java
+++ Java_practice/src/Chapter03/Main.java
@@ -23,5 +23,39 @@ public class Main {
 		Object a10 = new Sample(10);
 		Object b10 = new Sample(10);
 		System.out.println(a10.equals(b10));
+		
+		Object a11 = new Object();
+		Object b11 = null;
+		System.out.println(a11.equals(b11));
+		
+		String a12 = "sample";
+		String b12 = "sample";
+		System.out.print(a12 == b12);
+		System.out.print(", ");
+		System.out.println(a12.equals(b12));
+		
+		String a13 = new String ("sample");
+		String b13 = "sample";
+		System.out.print(a13 == b13);
+		System.out.print(", ");
+		System.out.println(a13.equals(b13));
+		
+		String a14 = "abc";
+		String b14 = new String(a14);
+		int count = 0;
+		if (a14.intern() == "abc") {
+			count++;
+		}
+		if (b14.intern() == "abc") {
+			count++;
+		}
+		if (a14.intern() == b14.intern()) {
+			count++;
+		}
+		System.out.println(count);
 	}
 }
+
+
+
+
